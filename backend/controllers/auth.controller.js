8c8692bec43ce230cbdb4076828e1b9e2a2ff9c7
Supratim Mandal <supratimm98@gmail.com>
2026-01-15 16:01:11 +0530

    Update cookie settings for token in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -58,8 +58,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true in production
-      sameSite: "lax",
+      secure: true, // true in production
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
